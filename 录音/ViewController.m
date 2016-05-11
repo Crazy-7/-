964063b08d6c3e69015808ac6e6bae7dc19e92b7
shiqilong <shiqilong@dslb-188-110-001-066.188.110.pools.vodafone-ip.de>
2016-05-11 11:07:45 +0800

    Initial Commit

---
+++ 录音/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  录音
+//
+//  Created by shiqilong on 16/5/11.
+//  Copyright © 2016年 shiqilong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
